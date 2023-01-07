960c00fa43f8be7bdcbd1081116c80884a3ca2e5
Asmamaw Damte <122156542+printAsmamaw@users.noreply.github.com>
2023-01-07 19:14:28 +0400

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+function Header() {
+  return (
+    <div className='header'>
+        hello
+   {/* <MenuIcon /> */}
+    </div>
+  )
+}
+
+export default Header
